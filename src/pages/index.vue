802e1ca09cf572fe87d0210a2c7a811ae001f971
Подковыров Константин Максимович <k.podkovyrov@efko.ru>
2024-08-07 11:52:55 +0300

    Настроил редирект

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { navigateTo } from 'nuxt/app';
+export default defineComponent({
+  name: 'MainPage',
+
+  setup() {
+    navigateTo('/auth/login')
+  },
+})
+</script>
